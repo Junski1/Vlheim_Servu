acb19d0c5367a91093ddf2a0a0729a7a67aad2da
Junski1 <58052011+Junski1@users.noreply.github.com>
2022-12-19 19:12:33 +0200

    save

--- StartValheim.bat
+++ StartValheim.bat
@@ -1,7 +1,8 @@
 cd C:\Users\%USERNAME%\AppData\LocalLow\IronGate\Valheim
 
 IF EXIST worlds_local\Vlheim_Servu\community.fwl (
-	move /y worlds_local\Vlheim_Servu\community* worlds_local
-	echo Done!)
+	IF EXIST worlds_local\Vlheim_Servu\community.db (
+		move /y worlds_local\Vlheim_Servu\community* worlds_local
+		echo Done!))
 ELSE(
 	echo Server files doesn't exist!)
--- StopValheim.bat
+++ StopValheim.bat
@@ -1,11 +1,13 @@
 cd C:\Users\%USERNAME%\AppData\LocalLow\IronGate\Valheim
 
 IF EXIST worlds_local\community.fwl (
-	IF EXIST worlds_local\community.fwl.old (
-		del *.old
-		echo Done!) ELSE (
-		echo Old Files doesn't exist!)
+	IF EXIST worlds_local\community.db (
+		IF EXIST worlds_local\community.fwl.old (
+			del worlds_local\*.old
+			echo Done!) ELSE (
+			echo Old Files doesn't exist!)
 
-	move /y worlds_local\community* worlds_local\Vlheim_Servu\) ELSE (
-
+		move /y worlds_local\community.db worlds_local\Vlheim_Servu\
+		move /y worlds_local\community.fwl worlds_local\Vlheim_Servu\
+)) ELSE (
 	echo Server files doesn't exist!)
