c8026bac5554d46dc3300cb187191bbce11a2f9d
Junski1 <58052011+Junski1@users.noreply.github.com>
2022-12-22 16:35:23 +0200

    Update StopValheim.bat

--- StopValheim.bat
+++ StopValheim.bat
@@ -4,10 +4,25 @@ IF EXIST worlds_local\community.fwl (
 	IF EXIST worlds_local\community.db (
 		IF EXIST worlds_local\community.fwl.old (
 			del worlds_local\*.old
-			echo Done!) ELSE (
+			echo Done!)ELSE (
 			echo Old Files doesn't exist!)
 
 		move /y worlds_local\community.db worlds_local\Vlheim_Servu\
 		move /y worlds_local\community.fwl worlds_local\Vlheim_Servu\
-)) ELSE (
+))ELSE (
 	echo Server files doesn't exist!)
+
+::IF EXIST worlds_local\community_auto_backup*.db(
+::	IF EXIST worlds_local\community_auto_backup*.fwl(
+
+::		set fileDate = dir /T:W worlds_local\community_auto_backup*.db
+
+::		IF EXIST worlds_local\Vlheim_Servu\backups\%fileDate%\(
+
+::		move /y worlds_local\community_auto_backup* worlds_local\Vlheim_Servu\backups
+::)ELSE(
+::		md worlds_local\Vlheim_Servu\backups\%fileDate%
+::		move /y worlds_local\community_auto_backup* worlds_local\Vlheim_Servu\backups
+::)))ELSE(
+::	echo Backups doesn't exist!
+::)
